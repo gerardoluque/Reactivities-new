50d03bf6521088e5bb5248a0f1370dc00d357783
Gerardo Luque <gluque@bts.com.mx>
2022-07-06 21:29:35 -0700

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -28,7 +28,6 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -37,6 +36,16 @@ namespace API
             services.AddDbContext<DataContext>(opt => 
             {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .WithOrigins("http://localhost:3000");
+                });
             });
         }
 
@@ -53,6 +62,8 @@ namespace API
             // app.UseHttpsRedirection(); --not using https for the moment
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
